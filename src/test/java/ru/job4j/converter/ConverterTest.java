dc2f99bb144208232dc250c028b1fdef150abd49
Sergei Ivanov <66008363+fromiva@users.noreply.github.com>
2021-12-27 17:19:38 +0300

    1. Модульные тесты. [#159435] (поля тест-класса перенесены в тест-методы)

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -5,48 +5,48 @@ import static org.junit.Assert.*;
 
 public class ConverterTest {
 
-    float in;
-    float expected;
-    float out;
-    float eps = 0.0001F;
-
     @Test
     public void whenConvert140RblThen2Euros() {
-        in = 140.0F;
-        expected = 2.0F;
-        out = Converter.roubleToEuro(in);
+        float in = 140.0F;
+        float expected = 2.0F;
+        float eps = 0.0001F;
+        float out = Converter.roubleToEuro(in);
         assertEquals(expected, out, eps);
     }
 
     @Test
     public void whenConvert174RblThen2Dot48572Euros() {
-        in = 174.0F;
-        expected = 2.48572F;
-        out = Converter.roubleToEuro(in);
+        float in = 174.0F;
+        float expected = 2.48572F;
+        float eps = 0.0001F;
+        float out = Converter.roubleToEuro(in);
         assertEquals(expected, out, eps);
     }
 
     @Test
     public void whenConvert0RblThen0Euros() {
-        in = 0.0F;
-        expected = 0.0F;
-        out = Converter.roubleToEuro(in);
+        float in = 0.0F;
+        float expected = 0.0F;
+        float eps = 0.0001F;
+        float out = Converter.roubleToEuro(in);
         assertEquals(expected, out, eps);
     }
 
     @Test
     public void whenConvert180RblThen3Dollars() {
-        in = 180.0F;
-        expected = 3.0F;
-        out = Converter.roubleToDollar(in);
+        float in = 180.0F;
+        float expected = 3.0F;
+        float eps = 0.0001F;
+        float out = Converter.roubleToDollar(in);
         assertEquals(expected, out, eps);
     }
 
     @Test
     public void whenConvert0RblThen0Dollars() {
-        in = 0.0F;
-        expected = 0.0F;
-        out = Converter.roubleToDollar(in);
+        float in = 0.0F;
+        float expected = 0.0F;
+        float eps = 0.0001F;
+        float out = Converter.roubleToDollar(in);
         assertEquals(expected, out, eps);
     }
 }
