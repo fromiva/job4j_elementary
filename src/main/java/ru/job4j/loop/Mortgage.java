2449cac27c9728e2c01506a2c8872a2eaca41fb3
fromiva <66008363+fromiva@users.noreply.github.com>
2022-01-21 11:39:45 +0300

    5.7. Ипотека [#156315 #253703]. Исключена промежуточная переменная

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        double fraction = percent / 100.0 + 1.0;
         while (amount > 0.0) {
-            amount = amount * fraction - salary;
+            amount = amount + amount * percent / 100.0 - salary;
             year++;
         }
         return year;
