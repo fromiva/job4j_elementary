5a926d74d34c5f72f1dedf1211a546cbc3e3a0cd
fromiva <66008363+fromiva@users.noreply.github.com>
2022-03-10 17:54:21 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,21 +1,28 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 public class Point {
 
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow((this.x - that.x), 2) + pow((this.y - that.y), 2));
     }
 
     public static void main(String[] args) {
 
-        double distance1 = distance(1, 1, 4, 5);
-        System.out.println("result (1, 1) to (4, 5) " + distance1);
-
-        double distance2 = distance(7, 15, -1, 2);
-        System.out.println("result (7, 15) to (-1, 2) " + distance2);
-
-        double distance3 = distance(100, 1020, 24, 95);
-        System.out.println("result (100, 1020) to (24, 95) " + distance3);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println("result (0, 0) to (0, 2) " + dist);
     }
 
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,52 +8,44 @@ public class PointTest {
 
     @Test
     public void when00To20Than2() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
 
         double expected = 2.0;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
 
         assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00To22Than2Dot83() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 2;
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 2);
 
         double expected = 2.83;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
 
         assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when71To29Than9Dot43() {
-        int x1 = 7;
-        int y1 = 1;
-        int x2 = 2;
-        int y2 = 9;
+        Point a = new Point(7, 1);
+        Point b = new Point(2, 9);
 
         double expected = 9.43;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
 
         assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when1001000To543Than107Dot35() {
-        int x1 = 100;
-        int y1 = 100;
-        int x2 = 54;
-        int y2 = 3;
+        Point a = new Point(100, 100);
+        Point b = new Point(54, 3);
 
         double expected = 107.35;
-        double out = Point.distance(x1, y1, x2, y2);
+        double out = a.distance(b);
 
         assertEquals(expected, out, 0.01);
     }
